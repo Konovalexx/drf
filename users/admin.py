611a5813fefac7dbd2017a0637237f44dede170f
Alex Con <konovalexx@gmail.com>
2024-10-15 00:03:27 +1000

    Курс 7 Домашка 3 DRF SkyPro 2 + админка для юзеров

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,21 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import User, Payment
+
+# Настройка админского интерфейса для кастомной модели пользователя
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone', 'city', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    search_fields = ('email', 'phone', 'city')
+    ordering = ('email',)
+
+# Настройка админского интерфейса для модели платежей
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'payment_date', 'course', 'lesson', 'amount', 'payment_method')
+    list_filter = ('payment_method', 'course', 'lesson')
+    search_fields = ('user__email', 'amount')
+    ordering = ('-payment_date',)
+
+# Регистрация моделей в админке
+admin.site.register(User, UserAdmin)
+admin.site.register(Payment, PaymentAdmin)
