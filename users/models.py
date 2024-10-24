09da9ab1bf007c9ed4c09b38c0280368e064143d
Alex Con <konovalexx@gmail.com>
2024-10-25 01:02:52 +1000

    Курс 7 Домашка 5 с services.py и исправленной вьюхой DRF SkyPro

--- users/models.py
+++ users/models.py
@@ -63,6 +63,7 @@ class Payment(models.Model):
     amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Сумма оплаты")
     payment_method = models.CharField(max_length=10, choices=PAYMENT_METHOD_CHOICES, verbose_name="Способ оплаты")
 
+    stripe_session_url = models.URLField(max_length=200, blank=True, null=True, verbose_name="Stripe Session URL")
     stripe_session_id = models.CharField(max_length=255, blank=True, null=True, verbose_name="Stripe Session ID")
 
     class Meta:
